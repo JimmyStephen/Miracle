e9eee422eefd81a207598f459774777df12270ec
Meliemau <hwhite@student.neumont.edu>
2023-04-19 11:44:00 -0600

    Finished animations... now to code

---
+++ Assets/Scripts/Gatcha/NewThings/PullManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullManager : MonoBehaviour
+{
+  [SerializeField] Animator light;
+
+  private void OnTriggerEnter(Collider other) {
+    
+  }
+}
